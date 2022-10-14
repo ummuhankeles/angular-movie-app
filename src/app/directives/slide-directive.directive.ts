e228fbcf91105d4abb92e67390c738ffd16c9303
Ümmühan Keleş <ummuhankeles4@gmail.com>
2022-10-14 14:20:57 +0300

    Add the smooth drag to the slide

--- src/app/directives/slide-directive.directive.ts
+++ src/app/directives/slide-directive.directive.ts
@@ -5,12 +5,21 @@ import { Directive, ElementRef, HostListener } from '@angular/core';
 })
 export class SlideDirectiveDirective {
 
+  stopper = true;
+
   constructor(private el: ElementRef) { }
 
   @HostListener('drag')
   prevFunction() {
-    let element = this.el.nativeElement.parentElement.parentElement.children[0];
-    let item = element.querySelectorAll(".slider-wrap-main-item");
-    element.prepend(item[item.length-1]);
+    if (this.stopper) {
+      this.stopper = false;
+      let element = this.el.nativeElement.parentElement.parentElement.children[0];
+      let item = element.querySelectorAll(".slider-wrap-main-item");
+      element.prepend(item[item.length-1]);
+      setTimeout(() => {
+        this.stopper = true;
+      }, 250)
+    }
+    
   }
 }
